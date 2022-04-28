1b321552aae1b587528094416ad13f9af61819f5
wis <is.jaras@gmail.com>
2022-04-28 18:18:25 -0400

    add test and fix alancito call

--- src/bots/alancito.py
+++ src/bots/alancito.py
@@ -17,12 +17,12 @@ GUILD = os.getenv('NEUROSISTEMAS_GUILD')
 #client = discord.Client()
 bot = commands.Bot(command_prefix='!')
 
-#@client.event
+
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
 
-#@client.event
+
 @bot.event
 async def on_member_join(member):
     """
@@ -33,27 +33,23 @@ async def on_member_join(member):
         f'Hi {member.name}, welcome to my Neurosistemas lab server!'
     )
 
-"""
-@client.event
-async def on_message(message):
-    if message.content == client.user.name:
-        response = "A su servicio {message.author} {message.__attr__}"
-
-        await message.channel.send(response)
-"""
 
 @bot.command()
 async def test(ctx, *args):
+    """
+    simple test
+    """
     await ctx.send('{} arguments: {}'.format(len(args), ', '.join(args)))
+
 
 @bot.command(name='Alancito')
 async def on_alancito(ctx):
     possible_response = [
-            f"A sus servicios Sr(a) {message.author.server_name}.",
+            f"A sus servicios Sr(a) {ctx.author.name}.",
             "yes, commander?"
             ]
     response = random.choice(possible_response) 
     await ctx.send(response)
 
-#client.run(TOKEN)
+
 bot.run(TOKEN)
