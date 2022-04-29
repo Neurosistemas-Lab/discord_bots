73d30be87d15b4fe012974ca666c7b1679bef5fc
wis <is.jaras@gmail.com>
2022-04-29 11:53:41 -0400

    add ping request

--- src/cogs/test.py
+++ src/cogs/test.py
@@ -7,7 +7,14 @@ class Test(discord.ext.commands.Cog, name='Test module'):
     @discord.ext.commands.command(name="test")
     async def test(self, ctx, *args):
         """
-        simple test function for bot testinf
+        simple test function for bot testing
         """
         await ctx.send('{} arguments: {}'.format(len(args), ', '.join(args)))
         
+
+    @discord.ext.commands.command(name="ping")
+    async def ping(self, ctx, *args):
+        """
+        ping request
+        """
+        await ctx.send(f"pong! still working ;)")
