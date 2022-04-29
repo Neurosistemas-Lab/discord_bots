2bb1e8fa0fc86bde0e1fdbec9ad32caa915091de
wis <is.jaras@gmail.com>
2022-04-29 00:46:50 -0400

    start working on add_birth

--- src/cogs/happy_birthday.py
+++ src/cogs/happy_birthday.py
@@ -1,15 +1,41 @@
 import discord
-from discord.ext import tasks
 import numpy as np
+import datetime
+
+from discord.ext import tasks
+from pathlib import Path
+
+# path to birthdays file
+cur_dir = Path.cwd()
+file_path = Path(cur_dir, "data", "birthdays.txt")
+BIRTHDAYS_FILE = file_path
 
 class HappyBirthday(discord.ext.commands.Cog, name='HappyBirthday module'):
     def __init__(self, bot):
         self.bot = bot
-        #self.birthdays = np.genfromtxt("", delimiter=",")
+        self.birthdays = np.genfromtxt(BIRTHDAYS_FILE, delimiter=",")
+        # maybe using a JSON its easier?
+        print(self.birthdays)
 
     @discord.ext.commands.command(name="add_birth")
     async def add_birth(self, ctx, *args):
-        self.birthdays.append([args["name"], args["date"]])
+
+        await ctx.send("Please enter your birthday in _day.month_ format")
+
+        # TODO: check same member answer to the bot
+        def check(msg):
+            return True
+
+        def find_date(msg):
+            """
+            find date information inside message object
+            """
+            pass
+
+        message = await self.bot.wait_for('message', check=check)
+        # TODO: find date inside message
+        date = find_date(message)
+        self.birthdays.append([ctx.author.name, date])
 
 
     @discord.ext.commands.command(name="list_birth")
