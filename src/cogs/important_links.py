9bfaaed1675c7b0a7537a2131b026c61b086f62e
wis <is.jaras@gmail.com>
2022-04-29 11:55:43 -0400

    improve documentation

--- src/cogs/important_links.py
+++ src/cogs/important_links.py
@@ -10,6 +10,9 @@ class ImportantLinks(discord.ext.commands.Cog, name='ImportantLinks module'):
 
     @discord.ext.commands.command(name="formulario_covid")
     async def formulario_covid(self, ctx):
+        """
+        formulario covid link request
+        """
         link = "http://formulariocovid.med.uchile.cl/index2.html " 
         response = f"{ctx.author.name} el link al formulario es: {link}" 
         await ctx.send(response)
@@ -17,6 +20,9 @@ class ImportantLinks(discord.ext.commands.Cog, name='ImportantLinks module'):
 
     @discord.ext.commands.command(name="zoom_labmeeting")
     async def zoom_labmeeting(self, ctx):
+        """
+        labmeeting zoom link request
+        """
         link = "https://uchile.zoom.us/j/85075112448?pwd=cnE3NGZjSUt6amZwT21rWUR6R050QT09" 
         response = f"{ctx.author.name} el link al zoom de la reunión del lab es: {link}" 
         await ctx.send(response)
@@ -24,6 +30,9 @@ class ImportantLinks(discord.ext.commands.Cog, name='ImportantLinks module'):
 
     @discord.ext.commands.command(name="zoom_jc")
     async def zoom_jc(self, ctx):
+        """
+        journal club link request
+        """
         link = "https://uchile.zoom.us/j/86803112745?pwd=c090alNNRHc2QXZ0U3AwQ2ExNk5QUT09" 
         response = f"{ctx.author.name} el link al zoom del JC es: {link}" 
         await ctx.send(response)
@@ -31,6 +40,9 @@ class ImportantLinks(discord.ext.commands.Cog, name='ImportantLinks module'):
 
     @discord.ext.commands.command(name="drive_jc")
     async def drive_jc(self, ctx):
+        """
+        journal club google's dirve link request
+        """
         link = "https://drive.google.com/drive/folders/1XS1BL0wy0hdP9AG6TWtu-z1hybKsG-zG" 
         response = f"{ctx.author.name} el link al drive del JC es: {link}" 
         await ctx.send(response)
