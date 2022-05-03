091d781f3a2a3673a83f56740f3067aa3cee0c9f
Camilo-UbuntuPC <camilo.jara@ug.uchile.cl>
2022-05-03 11:56:25 -0400

    minor changes for read csv

--- src/cogs/happy_birthday.py
+++ src/cogs/happy_birthday.py
@@ -1,54 +1,69 @@
 import discord
+import json
+import os
 import numpy as np
-import datetime
+import pandas as pd
+from datetime import datetime, timedelta
 
 from discord.ext import tasks
-from pathlib import Path
 
 # path to birthdays file
-cur_dir = Path.cwd()
-file_path = Path(cur_dir, "data", "birthdays.txt")
-BIRTHDAYS_FILE = file_path
+BIRTHDAYS_FILE = os.path.join(os.getcwd(), "data", "birthdays.csv")
+
+
 
 class HappyBirthday(discord.ext.commands.Cog, name='HappyBirthday module'):
     def __init__(self, bot):
         self.bot = bot
-        self.birthdays = np.genfromtxt(BIRTHDAYS_FILE, delimiter=",")
-        # maybe using a JSON its easier?
+        self.birthdays = pd.read_csv(BIRTHDAYS_FILE, 
+                                    delimiter=';', 
+                                    names=["id", "birth", "name"], 
+                                    header=None, 
+                                    index_col="id"
+        )
         print(self.birthdays)
 
     @discord.ext.commands.command(name="add_birth")
-    async def add_birth(self, ctx, *args):
+    async def add_birth(self, ctx, msg, *args):
+        await ctx.send("Please enter your birthday in dd/mm/yy format")
 
-        await ctx.send("Please enter your birthday in _day.month_ format")
-
-        # TODO: check same member answer to the bot
         def check(msg):
-            return True
-
-        def find_date(msg):
-            """
-            find date information inside message object
-            """
-            pass
-
+            return msg.author == ctx.author
+            
         message = await self.bot.wait_for('message', check=check)
-        # TODO: find date inside message
-        date = find_date(message)
-        self.birthdays.append([ctx.author.name, date])
+        try:
+            date = pd.to_datetime(message, dayfirst=True)
+            self.birthdays.loc[message.author.id] = [date, message.author.name]
+            await ctx.send("Birthday added!")
+        except:
+            await ctx.send("Something went wrong with your birthday format")
 
 
     @discord.ext.commands.command(name="list_birth")
     async def list_birth(self, ctx, *args):
-        pass
+        """
+        list all birthdays
+        """
+        await ctx.send(self.birthdays)
 
     @discord.ext.commands.command(name="remove_birth")
     async def remove_birth(self, ctx, *args):
-        pass
+        """
+        remove birthday
+        """
+        await ctx.send("Please enter your birthday in _day.month_ format")
+        message = await self.bot.wait_for('message', check=check)
+        # TODO: find date inside message
+        date = find_date(message)
+
 
     @discord.ext.commands.command(name="get_birth")
     async def get_birth(self, ctx, *args):
-        pass
+        """
+        get birthday
+        """
+        await ctx.send("Please enter your birthday in _day.month_ format")
+
 
     # is there a specific decorator for checking time??
     #https://discordpy.readthedocs.io/en/stable/ext/tasks/index.html
